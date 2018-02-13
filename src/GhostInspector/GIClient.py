037a699b8649e5a7df6288f46cb2fcf881ae599e
cameron <stilesc@wit.edu>
2018-02-12 23:40:26 -0500

    changes for cleaner reading of functions and tests connecting more parts

--- src/GhostInspector/GIClient.py
+++ src/GhostInspector/GIClient.py
@@ -1,43 +1,92 @@
 import sys
 import os
-
+import requests
 
 class Client(object):
     """A simple API client for the GhostInspector's API.
     see README for more details
     """
+
+
+    tests_name = []
+    test_suites = []
+    #last_update = 89    
+
+
     
-    def __init__(self, api_token=None, url_base="https://api.ghostinspector.com/"):
+    def __init__(self, api_token=None, url_base="https://api.ghostinspector.com/v1/"):
         self.api_token = api_token
         self.vars = "" #["startUrl" : "http://google.com",... ]
         self.url_base = url_base
-    
+        
     def get_test_results(self):
         """Retrieve the 
         """
         print("get_test_results")
-        return self._request("")
+        response = self._request("tests/")
+        print(response)
+        return response
     
-    def get_test(self):
+    def get_test_by_id(self, testid):
         """Retrieve the 
         """
-        return self._request("")
+        response = self._request("tests/<testId>/")
+        print(response)
+        return response
+
+    def get_id_by_test_name(self, testid):
+        """Retrieve the 
+        """
+        response = self._request("tests/<testId>/")
+        print(response)
+        return response
+   
+    def get_test_by_name(self, test_name):
+        """Retrieve the 
+        """ 
+        response = self._request("tests/<testId>/")
+        id = self.get_id_by_test_name(test_name)
+        self.get_test_by_id(id)
+       
+        return response
+    
+    def get_test_suites(self):
+        """Retrieve the 
+        """
+        response = self._request("tests/")
+        print(response)
+        return response
     
     def run_test(self):
         """Retrieve the 
         """
-        return self._request("")
+        response = self._request("tests/")
+        print(response)
+        return response
+
+    def get_tests(self):
+        """Retrieve the 
+        """
+        response = self._request("tests/")
+        print(response)
+        return response
     
     def run_tests(self):
         """Retrieve the 
         """
-        return self._request("")
+        response = self._request("tests/")
+        print(response)
+        return response
     
-    def get_test_results2(self):
+    def get_test_results(self):
         """Retrieve the 
         """
-        return self._request("")
+        response = self._request("tests/")
+        print(response)
+        return response
     
     def _request(self, path, parameters=None):
-        # Throw out parameters where the value is not None
-        return True
+
+        data = requests.get("%s%s?apiKey=%s" % (self.url_base, path, self.api_token))
+        print("%s%s?apiKey=%s" % (self.url_base, path, self.api_token))
+        return data.json()
--- src/tests/test_basic_calls.py
+++ src/tests/test_basic_calls.py
@@ -3,30 +3,45 @@
 Example
 """
 import sys
-
+import requests
 import os
 import pytest
 from src.GhostInspector.GIClient import Client
 
 
 def test_get_tests():
-    assert(True)
-
-def test_get_test(GIAPIClient):
-    GIAPIClient.get_test_results()
-    assert(True)
-
-def test_get_test_suites():
-    assert(True)
-
-def test_get_test_results():
-    assert(True)
     
-def test_get_test_details():
     assert(True)
 
-def test_get_tests_failure():
-    assert(True)
+def test_get_test_by_id(GIAPIClient):
+    a = GIAPIClient.get_test_by_id("99")
+    print(a)
+    assert(a != None)
+
+def test_get_test_by_name(GIAPIClient):
+    a = GIAPIClient.get_test_by_name("testees")
+    print(a)
+    assert(a != None)
+
+def test_get_test_results(GIAPIClient):
+    a = GIAPIClient.get_test_results()
+    print(a)
+    assert(a != None)
+
+def test_get_test_suites(GIAPIClient):
+    a = GIAPIClient.get_test_suites()
+    print(a)
+    assert(a != None)
+
+def test_get_test_details(GIAPIClient):
+    a = GIAPIClient.get_test_suites()
+    print(a)
+    assert(a != None)
+
+def test_get_tests_failure(GIAPIClient):
+    a = GIAPIClient.get_test_suites()
+    print(a)
+    assert(a != None)
     
-def test_generate_report():
+def test_generate_report(GIAPIClient):
     assert(True)
